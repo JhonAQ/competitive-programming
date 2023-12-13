be9100b5cb8272170099b1af1c843c72d5c1f1fa
JhonatanDczel <jariasq@unsa.edu.pe>
2023-12-13 02:00:43 -0500

    Commit inicial, creando el repositorio local

---
+++ blitz/readData1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int ts;
+  scanf("%d", &ts);
+  while (ts--) {
+    printf("Test case: %d \n", 10 - ts);
+  }
+  return 0;
+}
---
+++ blitz/readData2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int ts;
+  while(scanf("%d", &ts), ts){
+    printf("Test case: %d\n", 10 - ts);
+  }
+  return 0;
+}
