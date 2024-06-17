7042eee81b852dd88e67daadefd57fa840da6af4
JhonatanDczel <jariasq@unsa.edu.pe>
2024-06-16 21:20:33 -0500

    Add some problems

---
+++ cpp-basics/loops/N-numbers-histogram.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  char s; int n;
+  cin >> s >> n;
+  while(n--){
+    int x; cin >> x;
+    for(int i = 0; i < x; i++) cout << s;
+    cout << endl;
+  }
+}
