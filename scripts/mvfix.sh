a2cffdc407c75dbe66e153ed5b292a43af7a3644
JhonatanDczel <jariasq@unsa.edu.pe>
2024-06-17 15:00:14 -0500

    Avance script problem submit

---
+++ scripts/mvfix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+fileName=$1
+path="./".$2
+
+mv $fileName $path 
+
+read -p "Cree un nuevo problema local y presione una tecla para continuar"
+
+
+probFileSurce=$(find ./.cph -name "$fileName*" -type f)
+probFileDest=$(find ./$path/.cph -name "$fileName*" -type f)
+
