a85913235a14413349c18820e5357b2b1ed97e70
JhonatanDczel <jariasq@unsa.edu.pe>
2024-09-18 11:02:55 -0500

    Checkpoint, no se que hice hasta aqui xd

---
+++ contests/Math-02/D-The-Super-Powers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bitset<500> isP;
+
+int main(){
+  isP.set();
+  isP[1] = isP[0] = 0;
+  for(int i = 2; i < 501; i++){
+    if(isP[i]){
+      for(int j = i * i; j)
+    }
+  }
+}
