d750030b100e1a3cd5a685c0c78768916a4e1417
JhonatanDczel <jariasq@unsa.edu.pe>
2024-10-13 22:06:32 -0500

    Commit desde nvim

--- codeforces/intro-cp/A_Again_Twenty_Five.cpp
+++ codeforces/intro-cp/A_Again_Twenty_Five.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
   int n;
   cin >> n;
+  /*cout << "holaaa";*/
   if(n == 0) cout << "01";
   if(n == 1) cout << "05";
   if(n >= 2) cout << "25";
