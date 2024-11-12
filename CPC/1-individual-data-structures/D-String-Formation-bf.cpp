fd3ed180638d199a4f6f3355dca8b229003c5f16
JhonatanDczel <jariasq@unsa.edu.pe>
2024-11-11 19:02:22 -0500

    CPC D - String Formation by brute force mejorado, WA

--- CPC/1-individual-data-structures/D-String-Formation-bf.cpp
+++ CPC/1-individual-data-structures/D-String-Formation-bf.cpp
@@ -5,19 +5,21 @@ using namespace std;
 int main(){
   string str; cin >> str;
   int tc; cin >> tc;
+  int rev = 0;
   while(tc--){
     int o; cin >> o;
     if(o == 1){
-      reverse(str.begin(), str.end());
+      rev++;
     }else{
       int n; cin >> n;
       char c; cin >> c;
-      if(n == 1){
+      if(n == 1 || (rev & 1)){
         str = c + str;
       }else{
         str = str + c;
       }
     }
   }
+  if(rev & 1) reverse(str.begin(), str.end());
   cout << str << endl;
 }
