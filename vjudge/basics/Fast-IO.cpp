00ff10fb98ec054c0439d7ffdf398be6dfd454b4
JhonatanDczel <dev.dczel@gmail.com>
2025-02-25 10:39:34 -0500

    WFT? xd

--- vjudge/basics/Fast-IO.cpp
+++ vjudge/basics/Fast-IO.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main(){
+  ios::sync_with_stdio(false); cin.tie(NULL);
   int n; cin >> n; 
   while(n--){
     int a, b; cin >> a >> b;
-    cout << a * b << endl;
+    cout << a * b << '\n';
   }
 }
